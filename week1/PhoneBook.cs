24520a0158efae2c582ae8655217214d00cafc5b
clemexyo <ncihan@sabanciuniv.edu>
2023-12-11 17:50:41 +0300

    general structure

---
+++ week1/PhoneBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week1;
+
+public class PhoneBook
+{
+    private List<PhoneNumber> Numbers = new List<PhoneNumber>();
+
+    public void AddNumber(PhoneNumber number) { }
+    public void DeleteNumber(PhoneNumber number) { }
+    public void UpdateNumber(PhoneNumber number) { }
+    public void SortBook(string sorted) { }
+}
